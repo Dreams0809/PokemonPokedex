902ef7f926f8bea1945bb7f9e5c22b7e4371d94f
Dreams0809 <expensivepain9@gmail.com>
2022-12-04 12:02:10 -0500

    add port  command

--- server.js
+++ server.js
@@ -1,3 +1,9 @@
 ﻿const express = require('express')
 const app = express()
 const PORT = 3000
+
+
+
+app.listen(process.env.PORT || PORT, ()=>{
+    console.log(`Server running on port ${PORT}`)
+})
