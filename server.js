80566aa9cc51029f3e6103269144ee604b5d9f22
Dreams0809 <expensivepain9@gmail.com>
2022-12-20 19:46:36 -0500

    added user auth

--- server.js
+++ server.js
@@ -3,12 +3,13 @@ const app = express()
 const mongoose = require('mongoose')
 const passport = require('passport')
 const session = require('express-session')
-const MongoStore = require('connect-mongo')(session) 
+const MongoStore = require('connect-mongo')(session)
 const flash = require('express-flash')
 const logger = require('morgan')
 const connectDB = require('./config/database')
 const homeRoutes = require('./routes/home')
 const pokemonRoutes = require('./routes/pokemon')
+const cors = require('cors')
 require('dotenv').config({path: './config/.env'})
 
 
@@ -26,7 +27,7 @@ connectDB()
 
 
 // General Middleware
-
+app.use(cors())
 app.set('view engine', 'ejs')
 app.use(express.static('public'))
 app.use(express.urlencoded({ extended: true }))
@@ -40,7 +41,7 @@ app.use(
         secret: 'keyboard cat',
         resave: false,
         saveUninitialized: false,
-        store: new MongoStore.create({mongoUrl: process.env.DB_STRING})
+        store: new MongoStore({mongooseConnection: mongoose.connection})
     })
 )
 
