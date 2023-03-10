8b9c1593219ccc513e1373b7e9d77d608f8c2a28
Dreams0809 <expensivepain9@gmail.com>
2023-03-09 19:49:21 -0500

    nearly done

--- controllers/auth.js
+++ controllers/auth.js
@@ -49,7 +49,7 @@ const User = require('../Models/User')
   
   exports.getSignup = (req, res) => {
     if (req.user) {
-      return res.redirect('/')
+      return res.redirect('/profile')
     }
     res.render('signup', {
       title: 'Create Account'
@@ -89,7 +89,7 @@ const User = require('../Models/User')
           if (err) {
             return next(err)
           }
-          res.redirect('/')
+          res.redirect('/profile')
         })
       })
     })
